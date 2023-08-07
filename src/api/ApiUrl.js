929eefb9cd05e3ea7c066a314c6ee55ce01dc8e3
gurmeet00 <128692463+gurmeet00@users.noreply.github.com>
2023-08-07 11:00:26 +0530

    first commit

---
+++ src/api/ApiUrl.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:6767/api/";
+
+export const ApiURL = {
+  //*************
+  //User Api URL
+  //   ************
+  USER_REGISTRATION_URL: `${BASE_URL}`,
+  USER_LOGIN_URL: `${BASE_URL}`,
+};
